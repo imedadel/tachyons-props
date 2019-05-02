96a6f3bf828bc31118ddb6989d9230ae866df007
Imed Adel <aadelimed@gmail.com>
2019-05-02 17:00:54 +0100

    Remove theming from media queries

--- src/tachyons/media-queries.js
+++ src/tachyons/media-queries.js
@@ -1,9 +1,5 @@
 export const mediaQueries = {
-  breakpointNotSmall: `${p =>
-    p.theme.breakpointNotSmall || `screen and (min-width: 30em)`}`,
-  breakpointMedium: `${p =>
-    p.theme.breakpointMedium ||
-    `screen and (min-width: 30em) and (max-width: 60em)`}`,
-  breakpointLarge: `${p =>
-    p.theme.breakpointLarge || `screen and (min-width: 60em)`}`
+  breakpointNotSmall: `screen and (min-width: 30em)`,
+  breakpointMedium: `screen and (min-width: 30em) and (max-width: 60em)`,
+  breakpointLarge: `screen and (min-width: 60em)`
 };
